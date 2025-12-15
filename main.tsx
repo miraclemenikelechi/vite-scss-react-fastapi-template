7cc0cb5550b7f29d7fad9da0e10f43472bec1eab
miraclemenikelechi <miracle.emenike7@gmail.com>
2025-12-15 01:27:53 +0100

    refactor: remove default Vite template, clean up App components, update CSS import path

--- main.tsx
+++ main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./client/App";
-import "./client/index.css";
+import "./client/assets/index.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
